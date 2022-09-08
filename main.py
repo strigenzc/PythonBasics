85c480b02c8e2b36b18c4315912de847e27d53b0
Adam Schlicht <aschlicht@student.unimelb.edu.au>
2022-09-08 10:27:19 +1000

    update

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import functions_framework
+import random as rand
 
 @functions_framework.cloud_event
 def hello_cloud_event(cloud_event):
-   print(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
+    print(f"\nReceived event with ID: {cloud_event['id']} and data {cloud_event.data}\n")
 
-   
+    outcomes = { 'heads':0,
+             'tails':0,
+             }
+    sides = list(outcomes.keys())
+
+    for i in range(10000):
+        outcomes[ rand.choice(sides) ] += 1
+
+    print('Heads:', outcomes['heads'])
+    print('Tails:', outcomes['tails'])
--- request.sh
+++ request.sh
@@ -1,3 +1,4 @@
+# Call the Cloud Function
 curl -X POST localhost:8080 \
    -H "Content-Type: application/cloudevents+json" \
    -d '{
@@ -7,12 +8,13 @@ curl -X POST localhost:8080 \
 	"subject" : "123",
 	"id" : "A234-1234-1234",
 	"time" : "2018-04-05T17:31:00Z",
-	"data" : "hello world"
+	"data" : "This is a cloud function"
 }'
 
+# Deploy the Cloud Function
 gcloud functions deploy my-http-function \
   --region=us-central1 \
   --runtime=python39 \
   --source=. \
-  --entry-point=cloud_hello_event \
+  --entry-point=hello_cloud_event \
   --trigger-http
