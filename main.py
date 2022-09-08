ea99e734737fe6ed992f2ca1c18a1b057bf1f8ca
Adam Schlicht <aschlicht@student.unimelb.edu.au>
2022-09-08 10:40:35 +1000

    commenting

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import random as rand
 
 @functions_framework.cloud_event
 def hello_cloud_event(cloud_event):
+    """
+    Inputs:
+    Outputs:
+    What the function does
+    """
     print(f"\nReceived event with ID: {cloud_event['id']} and data {cloud_event.data}\n")
 
     outcomes = { 'heads':0,
              'tails':0,
              }
+
     sides = list(outcomes.keys())
 
     for i in range(10000):
